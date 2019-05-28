dc12c97e59d01745a1c7608bec49bd99763d2202
André <andre.carvalho1985@gmail.com>
2019-05-27 21:10:01 -0300

    disable turbolinks

--- app/assets/javascripts/tracker.js
+++ app/assets/javascripts/tracker.js
@@ -1,11 +1,11 @@
 if(localStorage) {
   let uuid = localStorage.getItem("_iCaseiUUID");
   if(uuid === null){
-      localStorage.setItem("_iCaseiUUID", uuidv4());
+      localStorage.setItem("_iCaseiUUID", generateUuidv4());
   }
 }
 
-function uuidv4() {
+function generateUuidv4() {
   return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
     (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
   )
@@ -19,4 +19,3 @@ fetch('http://localhost:3000/trackings', {
     uuid: localStorage.getItem("_iCaseiUUID")
   })
 });
-console.log("tracking")
